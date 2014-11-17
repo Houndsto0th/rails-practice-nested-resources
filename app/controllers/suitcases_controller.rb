d3baf4de41d9056c3f6e2ed3582b60f1952f9280
Houndsto0th <bry.kinnear@gmail.com>
2014-11-17 12:18:49 -0700

    created associations with nested resources

--- app/controllers/suitcases_controller.rb
+++ app/controllers/suitcases_controller.rb
@@ -1,49 +1,52 @@
 class SuitcasesController < ApplicationController
+  before_action do
+    @passenger = Passenger.find(params[:passenger_id])
+  end
 
   def index
-    @suitcases = Suitcase.all
+    @suitcases = @passenger.suitcases.all
   end
 
   def show
-    @suitcase = Suitcase.find(params[:id])
+    @suitcase = @passenger.suitcases.find(params[:id])
   end
 
   def new
-    @suitcase = Suitcase.new
+    @suitcase = @passenger.suitcases.new
   end
 
   def create
-    @suitcase = Suitcase.new(suitcase_params)
+    @suitcase = @passenger.suitcases.new(suitcase_params)
     if @suitcase.save
-      redirect_to suitcase_path(@suitcase)
+      redirect_to passenger_suitcases_path(@passenger)
     else
       render :new
     end
   end
 
   def edit
-    @suitcase = Suitcase.find(params[:id])
+    @suitcase = @passenger.suitcases.find(params[:id])
   end
 
   def update
-    @suitcase = Suitcase.find(params[:id])
+    @suitcase = @passenger.suitcases.find(params[:id])
     if @suitcase.update(suitcase_params)
-      redirect_to suitcase_path(@suitcase)
+      redirect_to passenger_suitcases_path(@passenger)
     else
       render :edit
     end
   end
 
   def destroy
-    @suitcase = Suitcase.find(params[:id])
+    @suitcase = @passenger.suitcases.find(params[:id])
     @suitcase.destroy
-    redirect_to suitcases_path
+    redirect_to passenger_suitcases_path(@passenger)
   end
 
   private
 
   def suitcase_params
-    params.require(:suitcase).permit(:description)
+    params.require(:suitcase).permit(:description, :passenger_id)
   end
 
 end
